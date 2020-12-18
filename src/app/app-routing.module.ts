187a3ff4316bf9a923e0e015235d8dbbb3ab6879
Vhen Larson A. Dela Cuesta <vhenlarsondelacuesta.1398@gmail.com>
2020-12-19 00:50:10 +0800

    Added Artist module to app router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
+import { InvokeMethodExpr } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full'},
   { path: 'auth',
-    loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule) },
-  // { path: 'artists' }
+    loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+  },
+  { path: 'artists',
+    loadChildren: () => import('./modules/artists/artists.module').then(m => m.ArtistsModule)
+  }
 ];
 
 @NgModule({
